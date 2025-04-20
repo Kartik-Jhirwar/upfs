724c2cad81bb9de664f6578353aa8af8f1a6dcd1
kartik.jhirwar <kartik.jhirwar@rapipay.com>
2025-04-21 01:39:30 +0530

    Corrected the url

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,17 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/upfs/",
+  base: "/upfs/", // Ensures that all paths are relative to /upfs/ when deployed on GitHub Pages
+  resolve: {
+    alias: [
+      { find: "@jumbo", replacement: "/src/@jumbo" },
+      { find: "@assets", replacement: "/src/@assets" },
+      { find: "@app", replacement: "/src/app" },
+    ],
+  },
   build: {
-    outDir: "dist", // Make sure this is the correct output directory
     rollupOptions: {
       output: {
-        // This ensures proper chunking and naming for assets
         manualChunks(id) {
           if (id.includes("node_modules")) {
             return id.toString().split("node_modules/")[1].split("/")[0]; // Separate chunks for node_modules
@@ -17,5 +22,6 @@ export default defineConfig({
         },
       },
     },
+    chunkSizeWarningLimit: 1000, // Increase the chunk size limit to 1MB
   },
 });
